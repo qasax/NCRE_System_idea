1cc4359690e99ae84a162557def40cd02f8985db
张法进 <2961360533@qq.com>
2023-07-10 17:34:22 +0800

    新建后台项目

---
+++ src/main/java/com/example/ncre_system_idea/NcreSystemIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ncre_system_idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcreSystemIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcreSystemIdeaApplication.class, args);
+    }
+
+}
