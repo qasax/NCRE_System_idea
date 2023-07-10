533c11af53daf4760ba0e4e497b11889622ab295
张法进 <2961360533@qq.com>
2023-07-10 17:34:22 +0800

    新建后台项目

---
+++ src/test/java/com/example/ncre_system_idea/NcreSystemIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ncre_system_idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcreSystemIdeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
