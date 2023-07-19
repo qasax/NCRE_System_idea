64bbc649fad333a25f3f11c85fd9771d3828ad28
仪子康 <1402376386@qq.com>
2023-07-19 10:59:59 +0800

    路径，读取请求携带的参数。 实现对 考试-考场 信息 增删查改 分页查询，条件查询

---
+++ src/main/java/com/example/ncre_system_idea/Controller/ExamRoomExamController.java
@@ -0,0 +1,43 @@
+package com.example.ncre_system_idea.Controller;
+
+import com.example.ncre_system_idea.Service.ExamRoomExamService;
+import com.example.ncre_system_idea.pojo.ExamRoomExam;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@CrossOrigin(origins = "http://localhost:8088", allowCredentials = "true")
+@RequestMapping("/examRoomExam")
+public class ExamRoomExamController {
+    @Autowired
+    ExamRoomExamService examRoomExamService;
+    @RequestMapping("/aLLExamroomExam")
+    @ResponseBody
+    public PageInfo<ExamRoomExam> selectALL(int pageNum, int pageSize, String sortProp, String sortOrder, boolean isSearch, String optionValue, String searchValue){
+
+        return examRoomExamService.selectAll(pageNum,pageSize,sortProp,sortOrder,isSearch,optionValue,searchValue);
+    }
+    @RequestMapping("/deleteOne")
+    @ResponseBody
+    public String deleteOne(int ereID){
+
+        return examRoomExamService.deleteOne(ereID);
+    }
+    @RequestMapping("/update")
+    @ResponseBody
+    public String update(@RequestBody ExamRoomExam examRoomExam){
+
+        return examRoomExamService.update(examRoomExam);
+    }
+    @RequestMapping("/addOne")
+    @ResponseBody
+    public String addOne(@RequestBody ExamRoomExam examRoomExam){
+
+        return examRoomExamService.addOne(examRoomExam);
+    }
+}
