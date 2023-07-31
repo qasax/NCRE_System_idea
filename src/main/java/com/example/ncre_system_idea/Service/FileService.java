3612daeae86ad32cc5d352cdf13e0198c511f7e6
张法进 <2961360533@qq.com>
2023-07-31 10:53:34 +0800

    解决华为云致命问题，使用try-with-resources语句，自动关闭资源

--- src/main/java/com/example/ncre_system_idea/Service/FileService.java
+++ src/main/java/com/example/ncre_system_idea/Service/FileService.java
@@ -44,40 +44,45 @@ public class FileService {
                     return list;
                 });
         //创建输入流
-        InputStream is = new FileInputStream(fileName);
-        //创建字节数组
-        byte[] bytes = new byte[is.available()];
-        //将流读到字节数组中
-        is.read(bytes);
-        //创建HttpHeaders对象设置响应头信息
-        MultiValueMap<String, String> headers = new HttpHeaders();
-        //设置要下载方式以及下载文件的名字
-        headers.add("Content-Disposition", "attachment;filename=exam.xlsx");
-        //设置响应状态码
-        HttpStatus statusCode = HttpStatus.OK;
-        //创建ResponseEntity对象
-        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, headers,statusCode);
-        //关闭输入流
-        is.close();
-        return responseEntity;
+        //使用try-with-resources语句自动关闭资源
+        try (InputStream is = new FileInputStream(fileName)) {
+            //创建字节数组
+            byte[] bytes = new byte[is.available()];
+            //将流读到字节数组中
+            is.read(bytes);
+            //创建HttpHeaders对象设置响应头信息
+            MultiValueMap<String, String> headers = new HttpHeaders();
+            //设置要下载方式以及下载文件的名字
+            headers.add("Content-Disposition", "attachment;filename=exam.xlsx");
+            //设置响应状态码
+            HttpStatus statusCode = HttpStatus.OK;
+            //创建ResponseEntity对象
+            ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, headers,statusCode);
+            //关闭输入流
+            is.close();
+            return responseEntity;
+        }
+
+
     }
     public String examUpload(@RequestParam MultipartFile file) throws IOException {
         // 写法1：JDK8+ ,不用额外写一个DemoDataListener
         // since: 3.0.0-beta1
         String fileName = "D:\\重装前桌面代码内容\\暑期实训_1\\ncre_Upload\\"+ "exam_" + ".xlsx";
-        OutputStream out = new FileOutputStream(fileName);
-        out.write(file.getBytes());
-        out.flush();
-        out.close();
-        // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
-        // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
-        EasyExcel.read(fileName, Exam.class, new PageReadListener<Exam>(dataList -> {
-            for (Exam exam : dataList) {
-                log.info("读取到一条数据{}", JSON.toJSONString(exam));
-                examDAO.addOne(exam);
-            }
-        })).sheet().doRead();
-        return "ok";
+        try(OutputStream out = new FileOutputStream(fileName)){
+            out.write(file.getBytes());
+            out.flush();
+            out.close();
+            // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
+            // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
+            EasyExcel.read(fileName, Exam.class, new PageReadListener<Exam>(dataList -> {
+                for (Exam exam : dataList) {
+                    log.info("读取到一条数据{}", JSON.toJSONString(exam));
+                    examDAO.addOne(exam);
+                }
+            })).sheet().doRead();
+            return "ok";
+        }
     }
     @Autowired
     StudentDAO studentDAO;
@@ -95,40 +100,42 @@ public class FileService {
                     return list;
                 });
         //创建输入流
-        InputStream is = new FileInputStream(fileName);
-        //创建字节数组
-        byte[] bytes = new byte[is.available()];
-        //将流读到字节数组中
-        is.read(bytes);
-        //创建HttpHeaders对象设置响应头信息
-        MultiValueMap<String, String> headers = new HttpHeaders();
-        //设置要下载方式以及下载文件的名字
-        headers.add("Content-Disposition", "attachment;filename=student.xlsx");
-        //设置响应状态码
-        HttpStatus statusCode = HttpStatus.OK;
-        //创建ResponseEntity对象
-        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, headers,statusCode);
-        //关闭输入流
-        is.close();
-        return responseEntity;
+        try(InputStream is = new FileInputStream(fileName)){
+            //创建字节数组
+            byte[] bytes = new byte[is.available()];
+            //将流读到字节数组中
+            is.read(bytes);
+            //创建HttpHeaders对象设置响应头信息
+            MultiValueMap<String, String> headers = new HttpHeaders();
+            //设置要下载方式以及下载文件的名字
+            headers.add("Content-Disposition", "attachment;filename=student.xlsx");
+            //设置响应状态码
+            HttpStatus statusCode = HttpStatus.OK;
+            //创建ResponseEntity对象
+            ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, headers,statusCode);
+            //关闭输入流
+            is.close();
+            return responseEntity;
+        }
     }
     public String studentUpload(@RequestParam MultipartFile file) throws IOException {
         // 写法1：JDK8+ ,不用额外写一个DemoDataListener
         // since: 3.0.0-beta1
         String fileName = "D:\\重装前桌面代码内容\\暑期实训_1\\ncre_Upload\\"+ "student_" + ".xlsx";
-        OutputStream out = new FileOutputStream(fileName);
-        out.write(file.getBytes());
-        out.flush();
-        out.close();
-        // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
-        // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
-        EasyExcel.read(fileName, Exam.class, new PageReadListener<Student>(dataList -> {
-            for (Student student : dataList) {
-                log.info("读取到一条数据{}", JSON.toJSONString(student));
-                studentDAO.addOne(student);
-            }
-        })).sheet().doRead();
-        return "ok";
+        try( OutputStream out = new FileOutputStream(fileName)){
+            out.write(file.getBytes());
+            out.flush();
+            out.close();
+            // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
+            // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
+            EasyExcel.read(fileName, Exam.class, new PageReadListener<Student>(dataList -> {
+                for (Student student : dataList) {
+                    log.info("读取到一条数据{}", JSON.toJSONString(student));
+                    studentDAO.addOne(student);
+                }
+            })).sheet().doRead();
+            return "ok";
+        }
     }
     @Autowired
     ProctorDAO proctorDAO;
@@ -146,40 +153,42 @@ public class FileService {
                     return list;
                 });
         //创建输入流
-        InputStream is = new FileInputStream(fileName);
-        //创建字节数组
-        byte[] bytes = new byte[is.available()];
-        //将流读到字节数组中
-        is.read(bytes);
-        //创建HttpHeaders对象设置响应头信息
-        MultiValueMap<String, String> headers = new HttpHeaders();
-        //设置要下载方式以及下载文件的名字
-        headers.add("Content-Disposition", "attachment;filename=proctor_.xlsx");
-        //设置响应状态码
-        HttpStatus statusCode = HttpStatus.OK;
-        //创建ResponseEntity对象
-        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, headers,statusCode);
-        //关闭输入流
-        is.close();
-        return responseEntity;
+        try(InputStream is = new FileInputStream(fileName)){
+            //创建字节数组
+            byte[] bytes = new byte[is.available()];
+            //将流读到字节数组中
+            is.read(bytes);
+            //创建HttpHeaders对象设置响应头信息
+            MultiValueMap<String, String> headers = new HttpHeaders();
+            //设置要下载方式以及下载文件的名字
+            headers.add("Content-Disposition", "attachment;filename=proctor_.xlsx");
+            //设置响应状态码
+            HttpStatus statusCode = HttpStatus.OK;
+            //创建ResponseEntity对象
+            ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, headers,statusCode);
+            //关闭输入流
+            is.close();
+            return responseEntity;
+        }
     }
     public String proctorUpload(@RequestParam MultipartFile file) throws IOException {
         // 写法1：JDK8+ ,不用额外写一个DemoDataListener
         // since: 3.0.0-beta1
         String fileName = "D:\\重装前桌面代码内容\\暑期实训_1\\ncre_Upload\\"+ "proctor_" + ".xlsx";
-        OutputStream out = new FileOutputStream(fileName);
-        out.write(file.getBytes());
-        out.flush();
-        out.close();
-        // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
-        // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
-        EasyExcel.read(fileName, Proctor.class, new PageReadListener<Proctor>(dataList -> {
-            for (Proctor proctor : dataList) {
-                log.info("读取到一条数据{}", JSON.toJSONString(proctor));
-                proctorDAO.addOne(proctor);
-            }
-        })).sheet().doRead();
-        return "ok";
+        try(OutputStream out = new FileOutputStream(fileName)){
+            out.write(file.getBytes());
+            out.flush();
+            out.close();
+            // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
+            // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
+            EasyExcel.read(fileName, Proctor.class, new PageReadListener<Proctor>(dataList -> {
+                for (Proctor proctor : dataList) {
+                    log.info("读取到一条数据{}", JSON.toJSONString(proctor));
+                    proctorDAO.addOne(proctor);
+                }
+            })).sheet().doRead();
+            return "ok";
+        }
     }
     @Autowired
     ExamRoomDAO examRoomDAO;
@@ -197,39 +206,41 @@ public class FileService {
                     return list;
                 });
         //创建输入流
-        InputStream is = new FileInputStream(fileName);
-        //创建字节数组
-        byte[] bytes = new byte[is.available()];
-        //将流读到字节数组中
-        is.read(bytes);
-        //创建HttpHeaders对象设置响应头信息
-        MultiValueMap<String, String> headers = new HttpHeaders();
-        //设置要下载方式以及下载文件的名字
-        headers.add("Content-Disposition", "attachment;filename=examRoom_.xlsx");
-        //设置响应状态码
-        HttpStatus statusCode = HttpStatus.OK;
-        //创建ResponseEntity对象
-        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, headers,statusCode);
-        //关闭输入流
-        is.close();
-        return responseEntity;
+        try(InputStream is = new FileInputStream(fileName)){
+            //创建字节数组
+            byte[] bytes = new byte[is.available()];
+            //将流读到字节数组中
+            is.read(bytes);
+            //创建HttpHeaders对象设置响应头信息
+            MultiValueMap<String, String> headers = new HttpHeaders();
+            //设置要下载方式以及下载文件的名字
+            headers.add("Content-Disposition", "attachment;filename=examRoom_.xlsx");
+            //设置响应状态码
+            HttpStatus statusCode = HttpStatus.OK;
+            //创建ResponseEntity对象
+            ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, headers,statusCode);
+            //关闭输入流
+            is.close();
+            return responseEntity;
+        }
     }
     public String examRoomUpload(@RequestParam MultipartFile file) throws IOException {
         // 写法1：JDK8+ ,不用额外写一个DemoDataListener
         // since: 3.0.0-beta1
         String fileName = "D:\\重装前桌面代码内容\\暑期实训_1\\ncre_Upload\\"+ "examRoom_" + ".xlsx";
-        OutputStream out = new FileOutputStream(fileName);
-        out.write(file.getBytes());
-        out.flush();
-        out.close();
-        // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
-        // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
-        EasyExcel.read(fileName, ExamRoom.class, new PageReadListener<ExamRoom>(dataList -> {
-            for (ExamRoom examRoom : dataList) {
-                log.info("读取到一条数据{}", JSON.toJSONString(examRoom));
-                examRoomDAO.addOne(examRoom);
-            }
-        })).sheet().doRead();
-        return "ok";
+        try(OutputStream out = new FileOutputStream(fileName)){
+            out.write(file.getBytes());
+            out.flush();
+            out.close();
+            // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
+            // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
+            EasyExcel.read(fileName, ExamRoom.class, new PageReadListener<ExamRoom>(dataList -> {
+                for (ExamRoom examRoom : dataList) {
+                    log.info("读取到一条数据{}", JSON.toJSONString(examRoom));
+                    examRoomDAO.addOne(examRoom);
+                }
+            })).sheet().doRead();
+            return "ok";
+        }
     }
 }
