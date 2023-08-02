3dfacfb77f2a60756a5fb157d4b3aa99fbfb24b1
张法进 <2961360533@qq.com>
2023-08-02 11:09:36 +0800

    增加一个拦截器，当用户处于离线状态时，提示用户需要重新登录

---
+++ src/main/java/com/example/ncre_system_idea/Interceptor/LoginInterceptor.java
@@ -0,0 +1,72 @@
+package com.example.ncre_system_idea.Interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 登录检查
+ * 1.配置到拦截器要拦截哪些请求
+ * 2.把这些配置放在容器中
+ *
+ * 实现HandlerInterceptor接口
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    /**
+     * 目标方法执行之前
+     * 登录检查写在这里，如果没有登录，就不执行目标方法
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//      获取进过拦截器的路径
+        String requestURI = request.getRequestURI();
+
+        //      登录检查逻辑
+        HttpSession session = request.getSession();
+        Object isLoginStatus =  session.getAttribute("isLoginStatus");
+        System.out.println("拦截器判断用户是否登录"+isLoginStatus);
+        if(isLoginStatus==null){//如果为null就说明该用户没有登录
+            response.setContentType("application/json;charset=UTF-8");
+            response.setStatus(401);
+            response.getWriter().write("用户已经离线，请重新登录");
+
+            return false;
+        }
+//
+        return true;
+    }
+
+    /**
+     * 目标方法执行完成以后
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    /**
+     * 页面渲染以后
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
